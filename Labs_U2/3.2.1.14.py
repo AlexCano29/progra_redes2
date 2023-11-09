31ea169831bcc2c33dfe4b358c3d1d7d2a725204
AlexCano29 <138088668+AlexCano29@users.noreply.github.com>
2023-11-08 21:56:20 -0600

    Laboratorios

---
+++ Labs_U2/3.2.1.14.py
@@ -0,0 +1,39 @@
+'''
+Autor: Julián Alexis Cano Cruces
+Fecha 8/11/23
+Ejercicio: Lab 3.2.1.14
+'''
+
+class Stack:
+    def __init__(self):
+        self.__stk = []
+
+    def push(self, val):
+        self.__stk.append(val)
+
+    def pop(self):
+        val = self.__stk[-1]
+        del self.__stk[-1]
+        return val
+
+
+class CountingStack(Stack):
+    def __init__(self):
+        super().__init__() 
+        self.__counter = 0 # Llena el constructor con acciones apropiadas.
+    
+
+    def get_counter(self):
+      return self.__counter
+    
+
+    def pop(self):
+        val = super().pop() 
+        self.__counter += 1 
+        return val    # Haz un pop y actualiza el contador.
+
+stk = CountingStack()
+for i in range(100):
+    stk.push(i)
+    stk.pop()
+print(stk.get_counter())
---
+++ Labs_U2/3.2.1.16.py
@@ -0,0 +1,36 @@
+'''
+Autor: Julián Alexis Cano Cruces
+Fecha 8/11/23
+Ejercicio: Lab 3.2.1.16
+''' 
+
+class QueueError(Exception):
+    pass
+
+class Queue:
+    def __init__(self):
+        self.__queue = []
+
+    def put(self, elem):
+        self.__queue.insert(0, elem)
+
+    def get(self):
+        if not self.__queue:
+            raise QueueError("La cola está vacía")
+        return self.__queue.pop()
+
+    def isempty(self):
+        return not bool(self.__queue)
+
+class SuperQueue(Queue):
+    pass
+
+que = SuperQueue()
+que.put(1)
+que.put("perro")
+que.put(False)
+for i in range(4):
+    if not que.isempty():
+        print(que.get())
+    else:
+        print("Cola vacía")
